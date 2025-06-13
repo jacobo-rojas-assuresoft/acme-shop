43ee5cf26a706c3d4afbd179b27ffc9a43e5eddb
jacobo-rojas-assuresoft <jacobo.rojas@assuresoft.com>
2025-06-13 02:58:03 -0400

    feat:configure Lint, Prettier and husky #5 close

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,16 +1,59 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import react from 'eslint-plugin-react';
+import pluginTs from '@typescript-eslint/eslint-plugin';
+import parserTs from '@typescript-eslint/parser';
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+export default [
+  js.configs.recommended,
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      react,
+      '@typescript-eslint': pluginTs,
+    },
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+      ecmaVersion: 2021,
+    },
+    rules: {
+      ...react.configs.flat?.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-no-target-blank': ['warn'],
+      'react/no-unescaped-entities': ['warn'],
+      'no-console': ['warn'],
+      'no-debugger': ['error']
+    },
+  },
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },    
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'prefer-const': ['warn'],
+    },
+  },
+
+  {
+    name: 'Prettier Rules',
+    ...prettier
+  }
 ];
-
-export default eslintConfig;
